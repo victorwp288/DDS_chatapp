744023af048898d85f96884954cbd33183830529
Victor Wejergang Petersen <victorwpetersen@gmail.com>
2025-04-11 09:04:52 +0200

    added nativewind v4 for styling

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import "./globals.css";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello DDS group!</Text>
+    <SafeAreaView className="flex-1 items-center justify-center bg-blue-500">
+      <Text className="text-red-600 text-2xl font-bold">Hello DDS group!</Text>
       <StatusBar style="auto" />
-    </View>
+
+	  	<Text className="border-lime-800 border-2">lol</Text>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
